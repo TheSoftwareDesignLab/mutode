ba1c5de17aba5ce7f8c688901743c873d39501c8
Diego Rodríguez <diegorbaquero@gmail.com>
2018-03-15 18:20:17 -0500

    Improve tests

--- test/test.js
+++ test/test.js
@@ -8,21 +8,25 @@ const opts = {}
 
 if (process.env.MUTODE_CONCURRENCY) opts.concurrency = process.env.MUTODE_CONCURRENCY
 
-let mutode = new Mutode(opts)
-
 test('Example module', async t => {
+  let mutode = new Mutode(opts)
+  t.is(mutode.mutants, 0)
+  t.is(mutode.killed, 0)
+  t.is(mutode.survived, 0)
+  t.is(mutode.discarded, 0)
   t.is(mutode.coverage, 0)
   await mutode.run()
-  t.true(mutode.killed > 0)
-  t.true(mutode.survived > 0)
-  t.true(mutode.discarded > 0)
-  t.true(mutode.coverage > 0)
+  t.is(mutode.mutants, 73)
+  t.is(mutode.killed + mutode.survived + mutode.discarded, mutode.mutants)
+  t.true(mutode.killed >= 53)
+  t.true(mutode.survived >= 19)
+  t.true(mutode.discarded >= 1)
+  t.true(mutode.coverage > 50)
 })
 
 test('Empty paths', async t => {
-  t.is(mutode.coverage, 0)
   t.throws(() => {
     const mutodeFail = new Mutode({paths: 'hello.js'})
-    mutode.run()
+    mutodeFail.run()
   })
 })
