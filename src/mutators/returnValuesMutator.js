6cac421cb58d7983d0c04dec79847c7d5fb04b11
Diego Rodríguez <diegorbaquero@gmail.com>
2018-04-05 21:30:29 -0500

    Add string values to return values mutator

--- src/mutators/returnValuesMutator.js
+++ src/mutators/returnValuesMutator.js
@@ -5,7 +5,10 @@ const mutantRunner = require('../mutantRunner')
 const lineDiff = require('../util/lineDiff')
 
 /**
- * @description Mutates return values. Negates booleans. Numbers > 0 are mutated to 0, 0 is mutated to 1.
+ * @description Mutates return values.
+ * Negates booleans.
+ * Numbers > 0 are mutated to 0, 0 is mutated to 1.
+ * String are mutated to an empty string. Empty string are mutated to a random string.
  * @function returnValuesMutator
  * @memberOf module:Mutators
  */
@@ -19,14 +22,21 @@ module.exports = async function returnValuesMutator ({mutodeInstance, filePath, 
       let mutantLineContent = lineContent
       switch (node.argument.type) {
         case 'BooleanLiteral': {
-          let newReturnValue = !node.argument.value
+          const newReturnValue = !node.argument.value
           mutantLineContent = lineContent.substr(0, node.argument.loc.start.column) +
             newReturnValue +
             lineContent.substr(node.argument.loc.end.column)
           break
         }
         case 'NumericLiteral': {
-          let newReturnValue = node.argument.value === 0 ? 1 : 0
+          const newReturnValue = node.argument.value === 0 ? 1 : 0
+          mutantLineContent = lineContent.substr(0, node.argument.loc.start.column) +
+            newReturnValue +
+            lineContent.substr(node.argument.loc.end.column)
+          break
+        }
+        case 'StringLiteral': {
+          const newReturnValue = node.argument.value.length === 0 ? Math.random().toString(36).replace(/[^a-z]+/g, '') : ''
           mutantLineContent = lineContent.substr(0, node.argument.loc.start.column) +
             newReturnValue +
             lineContent.substr(node.argument.loc.end.column)
