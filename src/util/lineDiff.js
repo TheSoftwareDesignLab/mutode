1f3b9ccff9a0b1690d5aee479bef63d0808b4b0e
Diego Rodríguez <diegorbaquero@gmail.com>
2018-04-08 18:49:45 -0500

    Use diff with words instead of chars

--- src/util/lineDiff.js
+++ src/util/lineDiff.js
@@ -2,7 +2,7 @@ const jsDiff = require('diff')
 const chalk = require('chalk')
 
 module.exports = (lineContent, mutantLineContent) => {
-  return jsDiff.diffChars(lineContent.trim(), mutantLineContent.trim()).map(stringDiff => {
+  return jsDiff.diffWords(lineContent.trim(), mutantLineContent.trim()).map(stringDiff => {
     if (stringDiff.added) return chalk.bgGreen(stringDiff.value)
     else if (stringDiff.removed) return chalk.bgRed(stringDiff.value)
     else return chalk.inverse(stringDiff.value)
